8db1001fa54832abf503a9ac6589bca0f530ed4b
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-05-19 21:13:12 +0500

    link JavaScript to the html through the id.

---
+++ app.js
@@ -0,0 +1,64 @@
+var CurrentNumber = document.getElementById("current-number");
+// var operatorSec = document.getElementById("operator-sec");
+// var previousNumber = document.getElementById("previous-number");
+// var result = document.getElementById("btn");
+// var deleteAll = document.getElementById("delete-all");
+// var delet = document.getElementById("delete");
+// var historySec = document.getElementById("history-sec");
+
+
+// function getValue(val) {
+//     var CurrentNumber = document.getElementById("current-number")
+//     CurrentNumber.value += val
+//     console.log(CurrentNumber.value)
+// }
+
+// function operatorFunction(opr) {
+//     if (CurrentNumber.value) {
+//         operatorSec.value = opr;
+//         previousNumber.value = CurrentNumber.value;
+//         CurrentNumber.value = "";
+//     } else {
+//         operatorSec.value = opr;
+//     }
+// }
+
+// function myResult() {
+//     var final = ""
+//     if (operatorSec.value == "+") {
+//         var num1 = Number(previousNumber.value);
+//         var num2 = Number(CurrentNumber.value);
+//         final = num1 + num2
+//         CurrentNumber.value += num2 + "="
+//         CurrentNumber.value = final
+//     } else if (operatorSec.value == "-") {
+//         var num1 = Number(previousNumber.value);
+//         var num2 = Number(CurrentNumber.value);
+//         final = num1 - num2
+//         operatorSec.value += num2 + "="
+//         operatorSec.value = final;
+//     } else if (operatorSec.value == "*") {
+//         var num1 = Number(previousNumber.value);
+//         var num2 = Number(CurrentNumber.value);
+//         final = num1 * num2
+//         operatorSec.value += num2 + "="
+//         CurrentNumber.value = final;
+//     } else if (operatorSec.value == "/") {
+//         var num1 = Number(previousNumber.value)
+//         var num2 = Number(CurrentNumber.value)
+//         final = num1 / num2
+//         operatorSec.value += num2 + "="
+//         CurrentNumber.value = final
+//     }
+// }
+
+// function deleteAllFunction() {
+//     CurrentNumber.value = "";
+//     operatorSec.value = "";
+//     previousNumber.value = "";
+// }
+
+// // single delete function ****
+// function deleteFunction() {
+//     CurrentNumber.value = CurrentNumber.value.slice(0, CurrentNumber.value.length - 1);
+// }
