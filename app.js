d791fff63e026b5ba1eba8c2bebccd0912a9ba39
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-05-19 21:14:24 +0500

    link JavaScript to the html through the id.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var CurrentNumber = document.getElementById("current-number");
 var operatorSec = document.getElementById("operator-sec");
-// var previousNumber = document.getElementById("previous-number");
+var previousNumber = document.getElementById("previous-number");
 // var result = document.getElementById("btn");
 // var deleteAll = document.getElementById("delete-all");
 // var delet = document.getElementById("delete");
